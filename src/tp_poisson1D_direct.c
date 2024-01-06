e1cc0ccc19b336fed436e88ae750c94e9da4de06
remyyyyh <remychiv.etu@gmail.com>
2024-01-06 14:43:44 +0100

    Ajout dgbsv_

--- src/tp_poisson1D_direct.c
+++ src/tp_poisson1D_direct.c
@@ -4,9 +4,12 @@
 /* to solve the Poisson 1D problem        */
 /******************************************/
 #include "lib_poisson1D.h"
-#include "../include/atlas_headers.h"
 
-int main(int argc, char *argv[])
+#define TRF 0
+#define TRI 1
+#define SV 2
+
+int main(int argc,char *argv[])
 /* ** argc: Nombre d'arguments */
 /* ** argv: Valeur des arguments */
 {
@@ -15,83 +18,89 @@ int main(int argc, char *argv[])
   int nbpoints, la;
   int ku, kl, kv, lab;
   int *ipiv;
-  int info;
+  int info = 1;
   int NRHS;
+  int IMPLEM = 0;
   double T0, T1;
   double *RHS, *EX_SOL, *X;
   double **AAB;
   double *AB;
 
-  double temp, relres;
+  double relres;
 
-  NRHS = 1;
-  nbpoints = 10;
-  la = nbpoints - 2;
-  T0 = -5.0;
-  T1 = 5.0;
+  if (argc == 2) {
+    IMPLEM = atoi(argv[1]);
+  } else if (argc > 2) {
+    perror("Application takes at most one argument");
+    exit(1);
+  }
+
+  NRHS=1;
+  nbpoints=10;
+  la=nbpoints-2;
+  T0=-5.0;
+  T1=5.0;
 
   printf("--------- Poisson 1D ---------\n\n");
-  RHS = (double *)malloc(sizeof(double) * la);
-  EX_SOL = (double *)malloc(sizeof(double) * la);
-  X = (double *)malloc(sizeof(double) * la);
+  RHS=(double *) malloc(sizeof(double)*la);
+  EX_SOL=(double *) malloc(sizeof(double)*la);
+  X=(double *) malloc(sizeof(double)*la);
 
   // TODO : you have to implement those functions
   set_grid_points_1D(X, &la);
-  set_dense_RHS_DBC_1D(RHS, &la, &T0, &T1);
+  set_dense_RHS_DBC_1D(RHS,&la,&T0,&T1);
   set_analytical_solution_DBC_1D(EX_SOL, X, &la, &T0, &T1);
-
+  
   write_vec(RHS, &la, "RHS.dat");
   write_vec(EX_SOL, &la, "EX_SOL.dat");
   write_vec(X, &la, "X_grid.dat");
 
-  kv = 1;
-  ku = 1;
-  kl = 1;
-  lab = kv + kl + ku + 1;
+  kv=1;
+  ku=1;
+  kl=1;
+  lab=kv+kl+ku+1;
 
-  AB = (double *)malloc(sizeof(double) * lab * la);
+  AB = (double *) malloc(sizeof(double)*lab*la);
 
   set_GB_operator_colMajor_poisson1D(AB, &lab, &la, &kv);
-
   write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "AB.dat");
 
-  // cblas_dgbmv(CblasColMajor, CblasNoTrans, lab, la, kl, ku, 1.0, AB, 1 + kl + ku,  /*vector*/ , 1, 0.0, AB, 1);
-
-
   printf("Solution with LAPACK\n");
+  ipiv = (int *) calloc(la, sizeof(int));
+
   /* LU Factorization */
-  info = 0;
-  ipiv = (int *)calloc(la, sizeof(int));
-  // dgbtrf_(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
+  if (IMPLEM == TRF) {
+    dgbtrf_(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
+  }
 
   /* LU for tridiagonal matrix  (can replace dgbtrf_) */
-  // ierr = dgbtrftridiag(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
-
-  // write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "LU.dat");
-
-  /* Solution (Triangular) */
-  if (info == 0)
-  {
-    // dgbtrs_("N", &la, &kl, &ku, &NRHS, AB, &lab, ipiv, RHS, &la, &info);
-    if (info != 0)
-    {
-      printf("\n INFO DGBTRS = %d\n", info);
-    }
+  if (IMPLEM == TRI) {
+    dgbtrftridiag(&la, &la, &kl, &ku, AB, &lab, ipiv, &info);
   }
-  else
-  {
-    printf("\n INFO = %d\n", info);
+
+  if (IMPLEM == TRI || IMPLEM == TRF){
+    /* Solution (Triangular) */
+    if (info==0){
+      dgbtrs_("N", &la, &kl, &ku, &NRHS, AB, &lab, ipiv, RHS, &la, &info);
+      if (info!=0){printf("\n INFO DGBTRS = %d\n",info);}
+    }else{
+      printf("\n INFO = %d\n",info);
+    }
   }
 
   /* It can also be solved with dgbsv */
-  // TODO : use dgbsv
+  if (IMPLEM == SV) {
+    dgbsv_(&la, &kl, &ku, &NRHS, AB, &lab, ipiv, RHS, &la, &info);
+  }
 
+
+  write_GB_operator_colMajor_poisson1D(AB, &lab, &la, "LU.dat");
   write_xy(RHS, X, &la, "SOL.dat");
 
   /* Relative forward error */
-  // TODO : Compute relative norm of the residual
-
-  printf("\nThe relative forward error is relres = %e\n", relres);
+  relres = relative_forward_error(RHS, EX_SOL, &la);
+  
+  printf("\nThe relative forward error is relres = %e\n",relres);
 
   free(RHS);
   free(EX_SOL);
